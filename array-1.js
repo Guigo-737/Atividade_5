9724abdfb227e12c900ffb9adb88793286cea173
Guigo-737 <rodrigohernandes@colegiopaulodetarso.com.br>
2025-06-02 16:17:43 +0000

    array

---
+++ array-1.js
@@ -0,0 +1,4 @@
+const marcas = ['nike', 'jordan', 'adidas', 'puma']
+console.log(marcas[0])
+console.log(marcas[marcas.length - 1])
+console.log(marcas.length)
---
+++ array-2.js
@@ -0,0 +1,4 @@
+const marcas = ['nike', 'jordan', 'mizuno']
+marcas[3] = 'Asics'
+console.log(marcas)
+console.log(marcas.length)
